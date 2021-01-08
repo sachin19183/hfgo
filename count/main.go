714c14f79bdcc72e717648f4437cb01561b41665
sachin19183 <sachin19183@gmail.com>
2021-01-08 01:04:40 +0100

    use map for count

--- count/main.go
+++ count/main.go
@@ -21,7 +21,40 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	} else {
-		fmt.Println(strList)
+		// now we count the occurrence of each unique expression in the data
+		//Method 1 via array slice
+		/*
+			var names []string
+			var countName []int
+			for _, line := range strList {
+				matched := false
+				for i, name := range names {
+					if name == line {
+						countName[i]++
+						matched = true
+					}
+				}
+				if matched == false {
+					names = append(names, line)
+					countName = append(countName, 1)
+				}
+			}
+		*/
+
+		//method 2 via map
+
+		var candidateCount = make(map[string]int)
+		for _, value := range strList {
+			candidateCount[value]++
+		}
+		/*
+			for i, value := range names {
+				fmt.Println(value, countName[i])
+			}
+		*/
+		for key, value := range candidateCount {
+			fmt.Println(key, ":", value)
+		}
 
 	}
 
